e3f9266c308d981dfa3a56221b66b5a11364bbf0
Jiogen <jiog3n@gmail.com>
2017-10-04 14:17:31 +0300

    Fix contact-list

--- LESSON_2.2/1-contact-list/index.js
+++ LESSON_2.2/1-contact-list/index.js
@@ -1,17 +1,9 @@
 'use strict';
-const contactList = [
-                      {'name': 'Daniel Day-Lewis', 'email': 'daylewis@gmail.com', 'phone': '+4 981 952 60 12'}, 
-                      {'name': 'Christoph Waltz', 'email': 'christoph@gmail.com', 'phone': '+6 944 1 77 60 95'}
-                    ];
-document.addEventListener('DOMContentLoaded', loadContacts);
-
-function loadContacts(){
-  const ulContactList = document.querySelector('.contacts-list');
-  let liString = '';
-  for (let i = 0; i < contactList.length; i++){
-    liString += '<li data-email="' + contactList[i].email + '" ';
-    liString += 'data-phone="' + contactList[i].phone + '">';
-    liString += '<strong>' + contactList[i].name + '</strong></li>';
-  } 
-  ulContactList.innerHTML = liString;
-};
+var contacts = loadContacts();
+contacts = JSON.parse(contacts);
+var contactsList = document.querySelector('.contacts-list');
+var li = contacts.reduce(function(prev, elem) {
+  console.log(prev)
+  return `${prev}<li data-email="${elem.email}" data-phone="${elem.phone}"><strong>${elem.name}</strong></li>`;
+}, '');
+contactsList.innerHTML = li;
