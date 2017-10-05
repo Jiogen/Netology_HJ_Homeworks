1d06ff6547339107abf9a357868233562758426e
Jiogen <jiog3n@gmail.com>
2017-10-05 10:49:59 +0300

    Feedback fix

--- LESSON_2.4/feedback/index.js
+++ LESSON_2.4/feedback/index.js
@@ -1,62 +1,59 @@
 'use strict';
-const inputs = document.querySelectorAll('.form-group input, .form-group textarea');
-const changeButton = document.querySelector('#output .button-contact');
-const contentform = document.querySelector('.contentform');
-const output = document.getElementById('output');
-let lastName = document.querySelector('.form-group input[name=lastname]'), 
-    name = document.querySelector('.form-group input[name=name]'), 
-    email = document.querySelector('.form-group input[name=email]'), 
-    company = document.querySelector('.form-group input[name=company]'), 
-    address = document.querySelector('.form-group input[name=address]'), 
-    zip = document.querySelector('.form-group input[name=zip]'),
-    city = document.querySelector('.form-group input[name=city]'),
-    phone = document.querySelector('.form-group input[name=phone]'),
-    role = document.querySelector('.form-group input[name=role]'),
-    subject = document.querySelector('.form-group input[name=subject]'),
-    message = document.querySelector('.form-group textarea[name=message]');
-let fieldsReady = false;
+const form = document.querySelector('.contentform'),
+    allInputs = form.querySelectorAll('input, textarea'),
+    zip = form.querySelector('input[name="zip"]'),
+    submit = form.querySelector('form.contentform > button.button-contact'),
+    mainOutput = document.querySelector('#output'),
+    allOutputs = mainOutput.querySelectorAll('output'),
+    changeButton = mainOutput.querySelector('.button-contact');
 
-zip.addEventListener('input', function(){
-  let value = this.value; 
-  let rep = /[-\.;":'a-zA-Zа-яА-Я]/; 
-  if (rep.test(value)) { 
-    value = value.replace(rep, ''); 
-    this.value = value; 
-  } 
+allInputs.forEach(function(elem) {
+  elem.addEventListener('input', onInput);
 });
 
-function checkFields(){
-  
-  if (lastName.value && name.value && email.value && company.value && address.value &&
-     zip.value && city.value && phone.value && role.value && subject.value && message.value) 
-    fieldsReady = true;
-  else 
-    fieldsReady = false; 
-  
-  if (fieldsReady) sendButton.disabled = false;
-  else sendButton.disabled = 'disabled';
+zip.addEventListener('input', onZipInput);
+
+function onInput() {
+  let isEmptyInput = false;
+  allInputs.forEach(function(elem) {
+    if (!elem.value) {
+      isEmptyInput = true;
+    }
+  }); 
+  submit.disabled = isEmptyInput;
 }
 
-for (let i = 0; i < inputs.length; i++){
-  inputs[i].addEventListener('input', checkFields);
+let zipOld = zip.value; 
+const allowedChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+function onZipInput(e) {
+  if ([...zip.value].every(elem => allowedChars.findIndex(c => c === elem) !== -1)) {
+    zipOld = zip.value;
+  } else {
+    zip.value = zipOld;
+    if (zip.empty) {
+      submit.disabled = true;
+    }    
+  }
 }
 
-contentform.addEventListener('submit', function(event){
-  event.preventDefault();
-  document.getElementById('name').value = name.value;
-  document.getElementById('lastname').value = lastName.value;
-  document.getElementById('company').value = company.value;  
-  document.getElementById('role').value = role.value;
-  document.getElementById('zip').value = zip.value;
-  document.getElementById('city').value = city.value;
-  document.getElementById('address').value = address.value;
-  document.getElementById('subject').value = subject.value;
-  document.getElementById('message').value = message.value;
-  contentform.classList.add('hidden');
-  output.classList.remove('hidden');
-});
+submit.addEventListener('click', onClickSubmit);
+changeButton.addEventListener('click', onClickChangeButton);
 
-changeButton.addEventListener('click', function(){
-  contentform.classList.remove('hidden');
-  output.classList.add('hidden');
-});
+function onClickSubmit(e) {
+  allOutputs.forEach(function(elem) {
+    let needInput = form.querySelector(`[name="${elem.id}"]`); 
+    if (needInput) {
+      elem.value = needInput.value;      
+    }
+  });
+  
+  form.classList.add('hidden');
+  mainOutput.classList.remove('hidden');
+  e.preventDefault();
+}
+
+function onClickChangeButton(e) {
+  form.classList.remove('hidden');
+  mainOutput.classList.add('hidden');
+  e.preventDefault();
+}
